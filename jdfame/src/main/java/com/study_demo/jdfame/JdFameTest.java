8ae41ee6be5fa9ff77d6f7380037f14595b2e685
zhanghaoyu01 <zhanghaoyu01@zhuanzhuan.com>
2024-09-29 19:23:06 +0800

    jdframe

---
+++ jdfame/src/main/java/com/study_demo/jdfame/JdFameTest.java
@@ -0,0 +1,32 @@
+package com.study_demo.jdfame;
+
+import com.study_demo.jdfame.baseinfo.StaticInfo;
+import com.study_demo.jdfame.baseinfo.Student;
+import io.github.burukeyou.dataframe.iframe.JDFrame;
+import io.github.burukeyou.dataframe.iframe.window.Sorter;
+import org.junit.Test;
+
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * @Description
+ * @Author Zhang hao yu
+ * @Date 2024/9/29 10:29
+ */
+public class JdFameTest {
+
+    @Test
+    public void basicTest(){
+        JDFrame<Student> read = JDFrame.read(StaticInfo.list);
+        read.show();
+        List<Student> students = read.toLists();
+
+//        System.out.println(read.columns());
+//        read.addRankCol(Sorter.toSorter(Comparator.comparingInt(Student::getAge))).show();
+        read.forEachDo(System.out::println);
+        read.forEachPreDo((prev, curr)->{
+        });
+
+    }
+}
---
+++ jdfame/src/main/java/com/study_demo/jdfame/baseinfo/Student.java
@@ -0,0 +1,17 @@
+package com.study_demo.jdfame.baseinfo;
+
+import lombok.Data;
+
+/**
+ * @Description
+ * @Author Zhang hao yu
+ * @Date 2024/9/29 10:56
+ */
+@Data
+public class Student {
+    private Integer id;
+    private String name;
+    private Integer age;
+    private Integer score;
+    private String school;
+}

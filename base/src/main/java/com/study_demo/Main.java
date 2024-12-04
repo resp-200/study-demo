1c7925fc7c7cfb8ce9c62db09ce43467d3b14eb4
zhanghaoyu01 <zhanghaoyu01@zhuanzhuan.com>
2024-12-04 19:24:14 +0800

    test

--- base/src/main/java/com/study_demo/Main.java
+++ base/src/main/java/com/study_demo/Main.java
@@ -8,20 +8,35 @@ import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
-        List<Double> list = new ArrayList<>();
-        list.add(1.0);
-        list.add(2.0);
+//        List<Double> list = new ArrayList<>();
+//        list.add(1.0);
+//        list.add(2.0);
+//
+//        List<Number> numbers = new ArrayList<>();
+//        List<Object> objects = new ArrayList<>();
+//        addNumber(list, numbers);
+//        addNumber2(list, objects);
 
-        List<Number> numbers = new ArrayList<>();
-        List<Object> objects = new ArrayList<>();
-        addNumber(list, objects);
-        addNumber2(list, objects);
+        print(1.0);
+        print("aaa");
+        print2(1.0);
+//        print2("aaa");
     }
+
+    public static <T> void print(T t){
+        System.out.println(t);
+    }
+    public static <T extends Number> void print2(T t){
+        System.out.println(t);
+    }
+
+
     public static void addNumber(List<? extends Number> list, List<Number> numbers){
         numbers.addAll(list);
     }
     public static void addNumber2(List<? extends Number> list, List<? super Number> numbers){
         numbers.addAll(list);
     }
+
 
 }

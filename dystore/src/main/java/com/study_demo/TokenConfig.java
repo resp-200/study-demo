ea55f4eb59c1ec607fdce20e3acd93d65cf4c097
zhanghaoyu01 <zhanghaoyu01@zhuanzhuan.com>
2024-12-24 16:30:09 +0800

    test

--- dystore/src/main/java/com/study_demo/TokenConfig.java
+++ dystore/src/main/java/com/study_demo/TokenConfig.java
@@ -8,8 +8,6 @@ public class TokenConfig {
 
     static {
         //设置appKey和appSecret，全局设置一次
-        GlobalConfig.initAppKey("7450825832362198538");
-        GlobalConfig.initAppSecret("4fd27f78-863b-4923-9c00-6f1e1a577eb3");
     }
 
     public static AccessToken getAccessToken(Long shopId) {
